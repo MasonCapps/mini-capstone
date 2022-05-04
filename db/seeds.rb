665af8da72efc5ad0f8c7e82850d7c65a76a7e98
Mason Capps <masoncapps42119@gmail.com>
2022-05-04 11:32:03 -0500

    Adds three products to the Product class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create(name: "TV", price: 400, image_url: "https://images.app.goo.gl/H8BpVUyyX8Ly5XML8", description: "Device to watch shows and movies.")
+Product.create(name: "Router", price: 100, image_url: "https://images.app.goo.gl/nzfqxU8FFGLpHg9Y8", description: "Device that supplies nearby devices with a wireless connection.")
+Product.create(name: "TV Remote", price: 10, image_url: "https://images.app.goo.gl/ZbHf4foCf85jeMdW6", description: "Device that controls the TV.")
