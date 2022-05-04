6861b741815b8bc9a18af532fca761c4145bd2d5
Mason Capps <masoncapps42119@gmail.com>
2022-05-04 11:42:32 -0500

    Adds route to display a single random product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
     all_products = Product.all
     render json: { Products: all_products }
   end
+
+  def single_product
+    product = Product.find_by(id: rand(1..3))
+    render json: { Product: product }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "display_products"
+
+  get "/product", controller: "products", action: "single_product"
 end
