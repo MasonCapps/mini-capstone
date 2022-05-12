1ec082d5caf88810e74635a5cdb4591db7293bd4
Mason Capps <masoncapps42119@gmail.com>
2022-05-12 11:53:33 -0500

    Adds the supplier information to each product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true
+  validates :name, uniqueness: true
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :description, presence: true
@@ -15,5 +16,9 @@ class Product < ApplicationRecord
 
   def total
     return price + tax
+  end
+
+  def supplier
+    Supplier.find_by(id: supplier_id)
   end
 end
