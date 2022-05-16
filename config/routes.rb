05c3edff9eae2c888b6ca3d3ad0b1120415277f5
Mason Capps <masoncapps42119@gmail.com>
2022-05-16 12:35:02 -0500

    Adds the create account and login paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
+  # Products routes
   get "/products/:id" => "products#show"
-
   get "/products" => "products#index"
-
   post "/products" => "products#create"
-
   patch "/products/:id" => "products#update"
-
   delete "/products/:id" => "products#destroy"
 
+  # Suppliers routes
   get "/suppliers/:id" => "suppliers#show"
-
   get "/suppliers" => "suppliers#index"
-
   post "/suppliers" => "suppliers#create"
-
   patch "/suppliers/:id" => "suppliers#update"
-
   delete "/suppliers/:id" => "suppliers#destroy"
+
+  # Authentication routes
+  post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
 end
