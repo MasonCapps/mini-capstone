17bd006bbfbd9b983fa1735a316d64e2d661b29a
Mason Capps <masoncapps42119@gmail.com>
2022-05-17 13:58:00 -0500

    Adds the universal variable that stores the jwt

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,29 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    user_orders = current_user.orders
-    render json: user_orders.as_json
+    @orders = current_user.orders
+    render template: "orders/index"
   end
 
   def show
-    order = current_user.orders.find_by(id: params[:id])
-    render json: order.as_json
+    @order = current_user.orders.find_by(id: params[:id])
+    render template: "orders/show"
   end
 
   def create
-    if current_user
-      product = Product.find_by(id: params[:product_id])
-      order = Order.new(
-        user_id: current_user.id,
-        product_id: params[:product_id],
-        quantity: params[:quantity],
-        subtotal: product.price * params[:quantity],
-        tax: product.tax * params[:quantity],
-        total: product.total * params[:quantity],
-      )
-      order.save
-      render json: order.as_json
-    else
-      render json: { message: "You must be logged in to create an order." }
-    end
+    product = Product.find_by(id: params[:product_id])
+    order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: product.price * params[:quantity],
+      tax: product.tax * params[:quantity],
+      total: product.total * params[:quantity],
+    )
+    order.save
+    render json: order.as_json
   end
 end
